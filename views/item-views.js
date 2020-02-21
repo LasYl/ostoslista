243ccf5017aaae69878d68142e485605863c531e
LasYl <56217678+LasYl@users.noreply.github.com>
2020-02-21 19:03:47 +0200

    Päivitys 2102

---
+++ views/item-views.js
@@ -0,0 +1,32 @@
+const item_view = ((data) => {
+    let html = `
+    <html>
+    <body>
+        Logged in as user: ${data.user_name}
+        <form action="/logout" method="POST">
+            <button type="submit">Log out</button>
+        </form>`;
+
+
+    data.items.forEach((item) => {
+        html += item.text;
+        html += `
+            <form action="delete-item" method="POST">
+                <input type="hidden" name="item_id" value="${item._id}">
+                <button type="submit">Delete item</button>
+            </form>
+            `;
+    });
+
+    html += `
+        <form action="/add-item" method="POST">
+            <input type="text" name="item">
+            <button type="submit">Add item</button>
+        </form>
+    </html>
+    </body>
+    `;
+    return html;
+});
+
+module.exports.item_view = item_view;
---
+++ views/list-views.js
@@ -0,0 +1,32 @@
+const list_view = ((data) => {
+    let html = `
+    <html>
+    <body>
+        Logged in as user: ${data.user_name}
+        <form action="/logout" method="POST">
+            <button type="submit">Log out</button>
+        </form>`;
+
+
+    data.lists.forEach((list) => {
+        html += list.text;
+        html += `
+            <form action="delete-list" method="POST">
+                <input type="hidden" name="list_id" value="${list._id}">
+                <button type="submit">Delete list</button>
+            </form>
+            `;
+    });
+
+    html += `
+        <form action="/add-list" method="POST">
+            <input type="text" name="list">
+            <button type="submit">Add list</button>
+        </form>
+    </html>
+    </body>
+    `;
+    return html;
+});
+
+module.exports.list_view = list_view;
