81d1136a97e162b9faa694bc07a8f1aeace2e2a3
LasYl <56217678+LasYl@users.noreply.github.com>
2020-02-28 16:58:11 +0200

    Muotoilua

--- views/item-views.js
+++ views/item-views.js
@@ -2,7 +2,6 @@ const item_view = ((data) => {
     let html = `
     <html>
     <head><title>Ostoslista</title>
-        
     <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
     <body>
@@ -11,26 +10,50 @@ const item_view = ((data) => {
         <form action="/logout" method="POST">
             <button type="submit">Log out</button>
             </div>
-        </form> <div class = "tausta">`;
+        </form> <div class = "tausta"><br>
+        <div><br>Back to shoppinglists<br></div>
+        <div><br>Shoppinglist: <br></div>
+
+        <table>
+        <th>Item</th><th>Quantity</th><th>Price</th><th></th>
+        `;
 
    
    data.items.forEach((item) => {
-        html += item.text; 
-        html += `
+        html += `<tr>
+            <td>${item.text}</td> 
+            <td>${item.quantity}</td> 
+            <td>${item.price}</td>
+            <td>
             <form action="/delete-item/" method="POST">
                 <input type="hidden" name="item_id" value="${item._id}">
                 <input type="hidden" name="list_id" value="${data.list_id}">
                 <button type="submit">Delete item</button>
-            </form>
+            </form></td>
+            </tr>
             `;
     });
 
     html += `
+        </table>
         <form action="/add-item/${data.list_id}" method="POST">
-            <input type="text" name="item_text">Item
-            <input type="number" name="quantity">Quantuty
-            <input type="number" placeholder="1.0" step="0.1" name="price">Price/
-            <button type="submit">Add item</button>
+        
+        <div style="float:left;margin-right:20px;">
+        <label for="Item_text">Item</label> <br>  
+        <input type="text" name="item_text">
+        <br>
+            
+        
+        <label for="quantity">Quantity</label><br>
+        <input type="number" name="quantity">
+        <br>
+
+        
+        <label for="price">Price</label> <br>
+        <input type="number" placeholder="1.0" step="0.1" name="price">€<br>
+        <br>
+
+        <br><button type="submit">Add item</button>
         </form>
         </div>
     </html>
